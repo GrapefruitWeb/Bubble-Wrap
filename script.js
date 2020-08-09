9b373bffba71de382d9a1b1dacb9550e26fc1a27
GrapefruitWeb <68019184+GrapefruitWeb@users.noreply.github.com>
2020-08-09 21:07:06 +0100

    Bubble Wrap!!!!!!!!
    AHHHH

---
+++ script.js
@@ -0,0 +1,37 @@
+// Thanks facebook
+var audio = 'https://cdn.fbsbx.com/hphotos-xaf1/v/t59.3654-21/11982748_10153645006268774_1506330817_n.mp3/pop.mp3.mp3?oh=808ff9045be18ee918fa6bdc12d1d8aa&oe=5627B5BD&dl=1'
+
+var width = window.innerWidth
+var height = window.innerHeight
+var radius = 12
+var size = radius * 2 + 'px'
+
+var el = d3.select('body').append('div')
+
+var x = 0
+var y = 0
+var isOdd = false
+while (true) {
+    if (y > height) {
+        break
+    }
+
+    el.append('span')
+        .style('left', x + 'px')
+        .style('top', y + 'px')
+        .style('width', size)
+        .style('height', size)
+        .on('mouseenter', function() {
+            d3.select(this)
+                .classed('poped', true)
+                .on('mouseenter', null)
+            new Audio(audio).play()
+        })
+
+    x += radius * 2 + 1
+    if (x > width) {
+        x = isOdd ? 0 : radius
+        isOdd = !isOdd
+        y += radius * 2 - 1
+    }
+}
